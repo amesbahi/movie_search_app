b87afc410727560cd32fbda260e94ac5e2d1652c
Alborz Mesbahi <alborzmesbahi@WTA-MBP163.local>
2017-02-07 22:38:26 -0500

    Use OMDb API to return movie data in JSON.

--- js/app.js
+++ js/app.js
@@ -1,26 +1,16 @@
 (function () {
     "use strict";
     // Variables
-    var searchInput = document.getElementById('search').value;
-    var yearInput = document.getElementById('year').value;
     var searchButton = document.getElementById('submit');
     var moviesList = document.getElementById('movies');
-
-    // What we ombdb API to return
-    var omdbRequestData = {
-        s: searchInput,
-        y: yearInput,
-        r: "json"
-    }
-
     var ombdbAPI = "http://www.omdbapi.com/?";
 
     // Success callback
     function success(data) {
         if (data.Response == true) {
             // Loop through and displays the movies from the response
-            var moviesHTML = '<li>';
             $.each(data, function (index, movie) {
+                var moviesHTML = '<li>';
                 moviesHTML += '<div class="poster-wrap">';
                 moviesHTML += '<img class="movie-poster" src="' + movie.Poster + '"></div>';
                 moviesHTML += '<span class="movie-title">' + movie.Title + '</span>';
@@ -30,9 +20,23 @@
         }
     };
 
-    searchButton.addEventListener("click", function (evt) {
-        evt.preventDefault();
+    searchButton.addEventListener("click", function (event) {
+        event.preventDefault();
+        if (moviesList.style.display == 'none') {
+            moviesList.style.display == 'block';
+        }
+        var searchInput = document.getElementById('search').value;
+        var yearInput = document.getElementById('year').value;
+        // What we want ombdb API to return
+        var omdbRequestData = {
+            s: searchInput,
+            y: yearInput,
+            r: "json",
+            plot: "full",
+            page: "1"
+        }
+        console.log(omdbRequestData);
         $.getJSON(ombdbAPI, omdbRequestData, success);
     }); // end submit click
-    
+
 } ()); // end module
