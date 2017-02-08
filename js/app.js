4b9a48c103f14640e05ceed16ac7a8cf70cf73eb
Alborz Mesbahi <alborzmesbahi@WTA-MBP163.local>
2017-02-07 23:01:06 -0500

    Getting back data from Search array.

--- js/app.js
+++ js/app.js
@@ -9,12 +9,12 @@
     function success(data) {
         if (data.Response == true) {
             // Loop through and displays the movies from the response
-            $.each(data, function (index, movie) {
+            $.each(data.Search, function (i, item) {
                 var moviesHTML = '<li>';
                 moviesHTML += '<div class="poster-wrap">';
-                moviesHTML += '<img class="movie-poster" src="' + movie.Poster + '"></div>';
-                moviesHTML += '<span class="movie-title">' + movie.Title + '</span>';
-                moviesHTML += '<span class="movie-year">' + movie.Year + '</span></li>';
+                moviesHTML += '<img class="movie-poster" src="' + item.Poster + '"></div>';
+                moviesHTML += '<span class="movie-title">' + item.Title + '</span>';
+                moviesHTML += '<span class="movie-year">' + item.Year + '</span></li>';
             });
             moviesList.innerHTML(moviesHTML);
         }
@@ -22,6 +22,7 @@
 
     searchButton.addEventListener("click", function (event) {
         event.preventDefault();
+        document.getElementsByClassName('desc')[0].style.display = 'none';
         if (moviesList.style.display == 'none') {
             moviesList.style.display == 'block';
         }
